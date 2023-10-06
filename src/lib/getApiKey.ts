0d3c12d73e027f5f791253d2657eae25233fb9fd
Марина <133772518+marinakokovihina@users.noreply.github.com>
2023-10-07 02:08:53 +0300

    clear comment

--- src/lib/getApiKey.ts
+++ src/lib/getApiKey.ts
@@ -1,11 +1,6 @@
 const apiKey = '7561b71ab2786e118fc948b4';
 
-// type getRateFromApi = {
-//     baseCode: string;
-//     targetCode: string;
-// }
 
-//а надо ли типизировать это таким образом если мы используем это не как функциональную компоненту??????
 export async function getRateFromApi(baseCode: string, targetCode: string){
 
     const urlRate = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${baseCode}/${targetCode}`;
@@ -13,6 +8,5 @@ export async function getRateFromApi(baseCode: string, targetCode: string){
 
     const dataRatePair = await ratePair.json();
     return dataRatePair.conversion_rate
-    //добавить проверку на ошибки о том что нихуя не пришло и тд
-} //получили число отношение из бейз кода к таргет коду и можем дальше с этой хуетой считать все
+}
 
